b654a8da8b2d4ca38e88432357cc033ca1022147
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-02-09 23:21:34 +0100

    feat: add assets and begin 'add reminder' page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'widgets/alarm_widget.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(children: [
+      body: Column(children: [
         const SizedBox(height: 50.0),
         Padding(
           padding: const EdgeInsets.only(right: 25.0),
@@ -46,42 +48,102 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: EdgeInsets.zero,
                 icon: const Icon(Icons.add),
                 color: Colors.black,
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (_, __, ___) => const AddReminderPage(),
+                        transitionDuration: const Duration(milliseconds: 100),
+                        transitionsBuilder: (context, animation, _, child) {
+                          return FadeTransition(
+                            opacity: animation,
+                            child: child,
+                          );
+                        },
+                      ));
+                },
               ),
             ),
           ),
         ),
         const SizedBox(height: 30.0),
-        const SizedBox(child:
-          Text('tOdAy',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 50.0,
-            fontFamily: 'Asgard',
-            letterSpacing: 4.0,
-          ),
+        const SizedBox(
+          child: Text(
+            'tOdAy',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 50.0,
+              fontFamily: 'Asgard',
+              letterSpacing: 4.0,
+            ),
           ),
         ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Stack(fit: StackFit.loose, children: [
-              Positioned(
-                  width: MediaQuery.of(context).size.width,
-                  bottom: 550.0,
-                  child: const AlarmWidget(
-                      color: Colors.red, time: '10:00', title: 'Wake up')),
-              Positioned(
-                  width: MediaQuery.of(context).size.width,
-                  bottom: 350.0,
-                  child: const AlarmWidget(
-                      color: Colors.green, time: '12:00', title: 'Lunch')),
-              Positioned(
-                  width: MediaQuery.of(context).size.width,
-                  bottom: 150.0,
-                  child: const AlarmWidget(
-                      color: Colors.blue, time: '18:00', title: 'Dinner')),
-            ]),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(children: const []),
           ),
+        ),
+      ]),
+    );
+  }
+}
+
+class AddReminderPage extends StatelessWidget {
+  const AddReminderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          height: MediaQuery.of(context).size.height,
+          constraints: const BoxConstraints.expand(),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background.jpg'),
+              fit: BoxFit.cover,
+            ),
+          )
+        ),
+        Column(
+          children: [
+            const SizedBox(height: 50.0),
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30,
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: const Icon(Icons.arrow_back),
+                      color: Colors.black,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      'Add Reminder',
+                      style: GoogleFonts.quicksand(
+                        textStyle: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                // Center text
+              ],
+            ),
+          ],
+        ),
       ]),
     );
   }
