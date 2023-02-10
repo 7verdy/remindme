22a837cdd2ff163ac3177d2dd448b9392952f82b
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-02-10 12:15:51 +0100

    feat: continue home page.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'RemindMe',
       theme: ThemeData(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 1, 9, 32),
+        primaryColor: const Color.fromRGBO(26, 188, 156, 1),
+        scaffoldBackgroundColor: Colors.transparent,
       ),
       home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
@@ -36,19 +37,69 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: const [
-        SizedBox(
-          height: 200.0,
-          child: Calendar()),
-        SizedBox(
-          height: 50.0,
-          child: Center(
-            child: Text(
-              'Today',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
+      body: Stack(children: [
+        Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromRGBO(26, 188, 156, 1),
+                Color.fromRGBO(1, 9, 32, 1),
+              ],
+            ),
+          ),
+        ),
+        Column(children: [
+          const SizedBox(height: 180.0, child: Calendar()),
+          const SizedBox(height: 30.0),
+          Expanded(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(1, 9, 32, 1),
+                // border top left and top right
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50.0),
+                  topRight: Radius.circular(50.0),
+                ),
               ),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 12.0),
+                  child: Text(
+                    'Today',
+                    style: GoogleFonts.getFont('Yeseva One',
+                        textStyle: const TextStyle(
+                          color: Color.fromRGBO(26, 188, 156, 1),
+                          fontSize: 30.0,
+                        )),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]),
+        Positioned(
+          bottom: 5.0,
+          right: 5.0,
+          child: CircleAvatar(
+            radius: 30.0,
+            backgroundColor: const Color.fromRGBO(26, 188, 156, 1),
+            child: IconButton(
+              icon: const Icon(Icons.add),
+              color: const Color.fromRGBO(1, 9, 32, 1),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AddReminderPage(),
+                  ),
+                );
+              },
             ),
           ),
         ),
@@ -66,38 +117,45 @@ class AddReminderPage extends StatelessWidget {
       body: Stack(children: [
         Column(
           children: [
-            const SizedBox(height: 50.0),
             Stack(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0),
-                  child: IconButton(
-                    padding: EdgeInsets.zero,
-                    icon: const Icon(Icons.arrow_back),
-                    color: Colors.white,
-                    splashRadius: 0.1,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(1, 9, 32, 1),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: Text(
-                      'Add Reminder',
-                      style: GoogleFonts.getFont('Yeseva One',
-                          textStyle: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 30.0,
-                          )),
+                Column(children: [
+                  Stack(children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5.0, top: 42.0),
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        icon: const Icon(Icons.arrow_back_ios),
+                        color: Colors.white,
+                        splashRadius: 0.1,
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
                     ),
-                  ),
-                ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 55.0, top: 50.0),
+                      child: Text(
+                        'Add Reminder',
+                        style: GoogleFonts.getFont('Yeseva One',
+                            textStyle: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 28.0,
+                            )),
+                      ),
+                    ),
+                  ]),
+                ]),
                 // Center text
               ],
             ),
-            const SizedBox(height: 60.0),
           ],
         ),
       ]),
--- lib/widgets/calendar.dart
+++ lib/widgets/calendar.dart
@@ -36,101 +36,111 @@ class _CalendarState extends State<Calendar> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          const SizedBox(height: 30.0),
           Container(
-              height: 30,
-              margin: const EdgeInsets.only(left: 10),
-              alignment: Alignment.centerLeft,
-              child: RichText(
-                text: TextSpan(
-                  text: months[selectedDate.month - 1],
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w800,
-                      color: accent),
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: ' ${selectedDate.year.toString()}',
-                        style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.w400,
-                            color: accent)),
-                  ],
-                ),
-              )),
-          const SizedBox(height: 10),
-          Expanded(
-            child: SingleChildScrollView(
-              child: SizedBox(
-                  height: 80,
-                  child: SizedBox(
-                      child: ListView.separated(
-                    separatorBuilder: (BuildContext context, int index) {
-                      return const SizedBox(width: 10);
-                    },
-                    itemCount: 365,
-                    controller: scrollController,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (BuildContext context, int index) {
-                      return InkWell(
-                        onTap: () {
-                          setState(() {
-                            currentDateSelectedIndex = index;
-                            selectedDate =
-                                DateTime.now().add(Duration(days: index));
-                          });
-                        },
-                        child: Container(
-                          height: 80,
-                          width: 60,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: currentDateSelectedIndex == index
-                                  ? accent
-                                  : Colors.transparent),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                DateTime.now()
-                                    .add(Duration(days: index))
-                                    .day
-                                    .toString(),
-                                style: TextStyle(
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                    color: currentDateSelectedIndex == index
-                                        ? Colors.white
-                                        : accent),
-                              ),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                days[DateTime.now()
-                                            .add(Duration(days: index))
-                                            .weekday -
-                                        1]
-                                    .toString(),
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: currentDateSelectedIndex == index
-                                        ? Colors.white
-                                        : accent),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  ))),
-            ),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+                color: Colors.transparent,
+              ),
           ),
-        ],
-      ),
+          Column(
+          children: [
+            const SizedBox(height: 10.0),
+            Container(
+                height: 30,
+                margin: const EdgeInsets.only(left: 10),
+                alignment: Alignment.centerLeft,
+                child: RichText(
+                  text: TextSpan(
+                    text: months[selectedDate.month - 1],
+                    style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w800,
+                        color: Color.fromRGBO(1, 9, 32, 1)),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: ' ${selectedDate.year.toString()}',
+                          style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w400,
+                              color: Color.fromRGBO(1, 9, 32, 1))),
+                    ],
+                  ),
+                )),
+            const SizedBox(height: 10),
+            Expanded(
+              child: SingleChildScrollView(
+                child: SizedBox(
+                    height: 80,
+                    child: SizedBox(
+                        child: ListView.separated(
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const SizedBox(width: 10);
+                      },
+                      itemCount: 365,
+                      controller: scrollController,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index) {
+                        return InkWell(
+                          onTap: () {
+                            setState(() {
+                              currentDateSelectedIndex = index;
+                              selectedDate =
+                                  DateTime.now().add(Duration(days: index));
+                            });
+                          },
+                          child: Container(
+                            height: 80,
+                            width: 60,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: currentDateSelectedIndex == index
+                                    ? const Color.fromRGBO(1, 9, 32, 1)
+                                    : Colors.transparent),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  DateTime.now()
+                                      .add(Duration(days: index))
+                                      .day
+                                      .toString(),
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                      color: currentDateSelectedIndex == index
+                                          ? accent
+                                          : const Color.fromRGBO(1, 9, 32, 1)),
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  days[DateTime.now()
+                                              .add(Duration(days: index))
+                                              .weekday -
+                                          1]
+                                      .toString(),
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: currentDateSelectedIndex == index
+                                          ? accent
+                                          : const Color.fromRGBO(1, 9, 32, 1)),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ))),
+              ),
+            ),
+          ],
+        ),
+      ]),
     ));
   }
 }
